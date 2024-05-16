00c2bb08fedc1f09822a555f52fd8e938b3ed471
Daniel Mlynarczyk <Daniel.Mlynarczyk@cshark.com>
2024-05-16 13:33:07 +0200

    New way of getting vault secrets

--- TravelAgency/Program.cs
+++ TravelAgency/Program.cs
@@ -2,12 +2,29 @@ using AgencyService.Adapter.API;
 using AgencyService.Adapter.RabbitMQ;
 using AgencyService.Adapter.SQLServer;
 using AgencyService.Core.Application;
+using TravelAgency.SharedLibrary.Vault;
+using TravelAgency.SharedLibrary.Vault.Consts;
 
 var builder = WebApplication.CreateBuilder(args);
 
 if (builder.Environment.IsProduction())
 {
-    builder.Configuration.AddJsonFile("secrets/appsettings.Production.json", optional: true);
+    var vaultBuilder = new VaultFacadeBuilder();
+
+    var vaultFacade = vaultBuilder
+                        .SetToken(Environment.GetEnvironmentVariable(VaultEnvironmentVariables.Token))
+                        .SetPort(Environment.GetEnvironmentVariable(VaultEnvironmentVariables.Port))
+                        .SetHost(Environment.GetEnvironmentVariable(VaultEnvironmentVariables.Host))
+                        .SetSSL(false)
+                        .Build();
+
+    var rabbitMq = await vaultFacade.ReadRabbitMqSecretAsync();
+    var connectionString = await vaultFacade.ReadAgencyServiceConnectionStringSecretAsync();
+    var cognito = await vaultFacade.ReadCognitoSecretAsync();
+
+    builder.Configuration.AddInMemoryCollection(rabbitMq);
+    builder.Configuration.AddInMemoryCollection(connectionString);
+    builder.Configuration.AddInMemoryCollection(cognito);
 }
 
 builder.AddApiServices(typeof(Program).Assembly);
